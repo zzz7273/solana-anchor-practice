ddb381a776e9011e100b443317e0afb90cf21e89
zzz7273 <zzz7273@qq.com>
2025-05-29 03:27:49 +0800

    counter-pda

--- counter/anchor/counter-pda/programs/counter-pda/src/lib.rs
+++ counter/anchor/counter-pda/programs/counter-pda/src/lib.rs
@@ -1,9 +1,9 @@
 use anchor_lang::prelude::*;
 
-declare_id!("J8QQE7ZVgVDQNFehx4orLCuVJTieyCacDBADHv8AhHQC");
+declare_id!("TpMCvyK8qGEZv7TBmn6woXyrhNfryQaXzj2Y1XToHEq");
 
 #[program]
-pub mod counter2 {
+pub mod counter_pda {
     use super::*;
 
     pub fn create_counter(ctx: Context<CreateCounter>, max_value: u64, counter_name: String, only_owner: bool ) -> Result<()> {
@@ -13,20 +13,12 @@ pub mod counter2 {
         c.count = 0;
         c.max_value = max_value;
         c.counter_name = counter_name;
-        c.only_owner = only_owner; 
+        c.only_owner = only_owner;
         c.remark = String::new(); // String::from("");
 
         msg!("Counter PDA Created");
         msg!("Counter Name: { }", c.counter_name);
 
-        Ok(())
-    }
-
-    pub fn set_only_owner(ctx: Context<SetOnlyOwner>, counter_name: String, only_owner: bool) -> Result<()> {
-        let c = &mut ctx.accounts.counter;
-
-        c.only_owner = only_owner;
-        msg!("Counter Only Owner now is: {}", c.only_owner);
         Ok(())
     }
 
@@ -48,33 +40,25 @@ pub mod counter2 {
         Ok(())
     }
 
-    pub fn reset_counter(ctx: Context<Increment>) -> Result<()> {
+    pub fn set_only_owner(ctx: Context<SetOnlyOwner>, counter_name: String, only_owner: bool) -> Result<()> {
         let c = &mut ctx.accounts.counter;
 
-        if c.only_owner {
-            require!(ctx.accounts.owner.key() == ctx.accounts.user.key(), CounterError::OnlyOwner);
-        }
-
-        c.count = 0;
-
-        msg!("Counter Reset to: {}", c.count);
+        c.only_owner = only_owner;
+        msg!("Counter Only Owner now is: {}", c.only_owner);
         Ok(())
     }
 
     pub fn set_remark(ctx: Context<SetRemark>, counter_name: String, remark: String) -> Result<()> {
         let c = &mut ctx.accounts.counter;
-        
-        require!(remark.len() <= MAX_REMARK_LENGTH, CounterError::RemarkTooLong);
 
-        if c.only_owner {
-            require!(ctx.accounts.owner.key() == ctx.accounts.user.key(), CounterError::OnlyOwner);
-        }
+        require!(remark.len() <= MAX_REMARK_LENGTH, CounterError::RemarkTooLong);
 
         c.remark = remark;
 
         msg!("Counter Remark now is: {}", c.remark);
         Ok(())
     }
+
 }
 
 #[error_code]
@@ -86,6 +70,10 @@ enum CounterError {
     #[msg("Remark Too Long")]
     RemarkTooLong,
 }
+
+// 创建者可以控制权限: 是否允许其他人对计数器进行“累加”调用。  
+// 创建者自己可以对计数器清零，也可以修改备注信息，其他人不允许操作。
+// 创建计数器时需提供名称。
 
 const MAX_NAME_LENGTH: usize = 20;
 const MAX_REMARK_LENGTH: usize = 100;
@@ -114,32 +102,19 @@ const DISCRIMINATOR: usize = 8;
 pub struct CreateCounter<'info> {
   #[account(
     init,
-    seeds = [b"", counter_name.as_bytes(), owner.key().as_ref()],
+    seeds = [counter_name.as_bytes(), owner.key().as_ref()],
     bump,
     payer = owner,
     space = DISCRIMINATOR + CounterInfo::INIT_SPACE,
   )]
   pub counter: Account<'info, CounterInfo>,
-  
+
   #[account(mut)]
   pub owner: Signer<'info>,
 
   pub system_program: Program<'info, System>,
 }
 
-
-#[derive(Accounts)]
-#[instruction(counter_name:String)]
-pub struct SetOnlyOwner<'info> {
-  #[account(
-    mut, // 这里有坑, 如果没有mut，系统并不会报错，但数据没有发生变化!!!!
-    seeds = [counter_name.as_bytes(), owner.key().as_ref()],
-    bump
-  )]
-  pub counter: Account<'info, CounterInfo>,
-  
-  pub owner: Signer<'info>,
-}
 
 // https://www.anchor-lang.com/docs/references/account-types
 
@@ -155,27 +130,34 @@ pub struct Increment<'info> {
 
   /// CHECK: AccountInfo is an unchecked account
   pub owner: AccountInfo<'info>,  // 这里只需要获取账户的公钥.
-  
+
   pub user: Signer<'info>,
 }
 
+
 #[derive(Accounts)]
-#[instruction(first_param_is_name:String)]
+#[instruction(counter_name:String)]
+pub struct SetOnlyOwner<'info> {
+    #[account(
+    mut, // 这里有坑, 如果没有mut，系统并不会报错，但数据没有发生变化!!!!
+    seeds = [counter_name.as_bytes(), owner.key().as_ref()],
+    bump )]         
+    pub counter: Account<'info, CounterInfo>,
+    pub owner:Signer<'info>,
+}                                                                                                                                      
+
+#[derive(Accounts)]
+#[instruction(counter_name:String)]
 pub struct SetRemark<'info> {
   #[account(
     mut,
-    seeds = [first_param_is_name.as_bytes(), owner.key().as_ref()],
+    seeds = [counter_name.as_bytes(), owner.key().as_ref()],
     bump,
-        // realloc = DISCRIMINATOR + MovieAccountState::INIT_SPACE,
-        // realloc::payer = initializer,
-        // realloc::zero = true,
+    // realloc = DISCRIMINATOR + CounterInfo::INIT_SPACE,
+    // realloc::payer = initializer,
+    // realloc::zero = true,
   )]
   pub counter: Account<'info, CounterInfo>,
-  
-  /// CHECK: AccountInfo is an unchecked account
-  pub owner: AccountInfo<'info>,  // 这里只需要获取账户的公钥.
-  
-  pub user: Signer<'info>,
+
+  pub owner: Signer<'info>,
 }
-
-
--- counter/anchor/counter-pda/tests/counter-pda.ts
+++ counter/anchor/counter-pda/tests/counter-pda.ts
@@ -1,18 +1,19 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Counter2 } from "../target/types/counter2";
+import { CounterPda } from "../target/types/counter_pda";
+
 import { BN } from "bn.js";
 
 import { expect } from "chai";
 import { airdropIfRequired } from "@solana-developers/helpers";
 
-describe("counter2++++++++++++", () => {
+describe("counter-pda", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
-  
+
   anchor.setProvider(provider);
 
-  // 直接使用test框架默认的钱包，可能有一些默认行为不受控制。后面交易时还是指定钱包来签名吧
+  // 直接使用test框架默认的钱包，可能有一些默认行为不知道如何控制。后面交易时还是指定自定义钱包来签名吧
 
   const keypair1 = anchor.web3.Keypair.generate();
   console.log("wallet1 address:", keypair1.publicKey.toBase58());
@@ -22,7 +23,7 @@ describe("counter2++++++++++++", () => {
 
 
 
-  const program = anchor.workspace.counter2 as Program<Counter2>;
+  const program = anchor.workspace.counter_pda as Program<CounterPda>;
 
   const defineCounterByWallet1 = (name:string, maxVal: number, onlyOwner:boolean) => {
     const [pda, bump] = anchor.web3.PublicKey.findProgramAddressSync(
@@ -47,9 +48,9 @@ describe("counter2++++++++++++", () => {
     // Add your test here.
     console.log("\n\n");
 
-    await airdropIfRequired(anchor.getProvider().connection, keypair1.publicKey, 2_000_000_000, 1_000_000_000);
-    await airdropIfRequired(anchor.getProvider().connection, keypair1.publicKey, 2_000_000_000, 1_000_000_000);
-    
+    await airdropIfRequired(anchor.getProvider().connection, keypair1.publicKey, 2*anchor.web3.LAMPORTS_PER_SOL, 1*anchor.web3.LAMPORTS_PER_SOL);
+    await airdropIfRequired(anchor.getProvider().connection, keypair2.publicKey, 2*anchor.web3.LAMPORTS_PER_SOL, 1*anchor.web3.LAMPORTS_PER_SOL);
+
     // pda 通过 keypair1 派生
     const tx = await program.methods.createCounter(c1.maxValue, c1.counterName, c1.onlyOwner)
       .accountsPartial({counter:c1.pda,  owner: keypair1.publicKey})
@@ -67,33 +68,41 @@ describe("counter2++++++++++++", () => {
     expect(cc.onlyOwner).to.equal(true);
   });
 
+
+
   it("Is increament for owner!", async () => {
-    
+
     // https://solana.stackexchange.com/questions/16070/typescript-type-error-with-multiple-accounts-in-program-struct
     // https://github.com/solana-foundation/anchor/pull/2824    搜索"accountsPartial"
+
     // accounts: This method is now fully type-safe based on the resolution fields in the IDL, making it much easier to only specify the accounts that are actually needed.
     // accountsPartial: This method keeps the old behavior and let's you specify all accounts including the resolvable ones.
     // accountsStrict: If you don't want to use account resolution and specify all accounts manually (unchanged).
-    
+
     // Add your test here.
 
     console.log("\n\n");
+
+    const cc0 = await program.account.counterInfo.fetch(c1.pda);
+    console.log(`counter[${c1.pda}] chain info before increment: `, cc0);
 
     const tx = await program.methods
       .increment(c1.counterName)
       .accountsPartial({counter: c1.pda,owner: keypair1.publicKey, user: keypair1.publicKey})
       .signers([keypair1])
       .rpc();
-
     // console.log("create counter c2 signature", tx);
 
     const cc = await program.account.counterInfo.fetch(c1.pda);
 
     console.log(`counter[${c1.pda}] chain info: `, cc);
 
+    expect(cc.count.toNumber()).to.equal(1);
+
   });
 
   const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 
   it("Is increament for others should be failed!", async () => {
     console.log("\n\n");
@@ -102,7 +111,7 @@ describe("counter2++++++++++++", () => {
       const tx = await program.methods
         .increment(c1.counterName)
         .accountsPartial({counter: c1.pda, owner: keypair1.publicKey, user: keypair2.publicKey})
-        .signers([keypair2]) 
+        .signers([keypair2])
         .rpc();
 
 
@@ -123,9 +132,8 @@ describe("counter2++++++++++++", () => {
         // expect((err as AnchorError).error.errorMessage).to.equal("your error message");
         console.log("increment for others failed as expected:", (""+err).split("\n")[0]+" ...");
     }
-
-
   });
+
 
 
   it("Is increament for others should be success!", async () => {
@@ -134,16 +142,16 @@ describe("counter2++++++++++++", () => {
     const tx1 = await program.methods
       .setOnlyOwner(c1.counterName, false)
       .accountsPartial({counter: c1.pda, owner: keypair1.publicKey})
-      .signers([keypair1]) 
+      .signers([keypair1])
       .rpc();
-    
+
     const cc0 = await program.account.counterInfo.fetch(c1.pda);
     console.log(`counter00[${c1.pda}] chain info: `, cc0);
 
     const tx2 = await program.methods
       .increment(c1.counterName)
       .accountsPartial({counter: c1.pda, owner: keypair1.publicKey, user: keypair2.publicKey})
-      .signers([keypair2]) 
+      .signers([keypair2])
       .rpc();
 
 
@@ -152,6 +160,37 @@ describe("counter2++++++++++++", () => {
     const cc = await program.account.counterInfo.fetch(c1.pda);
 
     console.log(`counter[${c1.pda}] chain info: `, cc);
+
+    expect(cc.count.toNumber()).to.equal(2);
+
+  });
+
+
+
+
+  it("Is set remark!", async () => {
+
+    // Add your test here.
+
+    console.log("\n\n");
+
+    const cc0 = await program.account.counterInfo.fetch(c1.pda);
+    console.log(`counter[${c1.pda}] chain info before increment: `, cc0);
+
+    const sss = "计数器好玩吗？";
+
+    const tx = await program.methods
+      .setRemark(c1.counterName,sss)
+      .accountsPartial({counter: c1.pda,owner: keypair1.publicKey, owner: keypair1.publicKey})
+      .signers([keypair1])
+      .rpc();
+    // console.log("create counter c2 signature", tx);
+
+    const cc = await program.account.counterInfo.fetch(c1.pda);
+
+    console.log(`counter[${c1.pda}] chain info: `, cc);
+
+    expect(cc.remark).to.equal(sss);
 
   });
 
